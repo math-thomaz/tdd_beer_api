8fe8ec4b6267221cda719aad6f6a5cf4083951ea
Matheus Thomaz <matheustthomaz@gmail.com>
2021-08-24 15:22:01 -0300

    Project start!

---
+++ src/main/java/com/dio/beerapiproject/BeerapiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.dio.beerapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerapiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerapiprojectApplication.class, args);
+	}
+
+}
