fc502db9594e8a7db991d38a9877dbed438ed003
Matheus Thomaz <matheustthomaz@gmail.com>
2021-08-25 02:29:38 -0300

    Entity and DTO created.

---
+++ src/main/java/com/dio/beerapiproject/entity/Beer.java
@@ -0,0 +1,35 @@
+package com.dio.beerapiproject.entity;
+
+import com.dio.beerapiproject.enums.BeerType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Beer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private int max;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Enumerated
+    @Column(nullable = false)
+    private BeerType type;
+}
